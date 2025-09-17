95d9f0c570733d8ffbedbf74998b277d61f31705
Matheus Cuero <101690604+Oroboni@users.noreply.github.com>
2025-09-17 17:28:32 -0300

    a

---
+++ Atividade17/exercicios/app.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let app = express();
+
+app.get('/', function(req, res){
+    res.send("<html><body>Site da Fatec Sorocaba</body></html>")
+});
+app.get('/historia', function(req, res){
+    res.send("<html><body>História da Fatec Sorocaba</body></html>")
+});
+
+app.get('/cursos', function(req, res){
+    res.send("<html><body>Cursos da Fatec Sorocaba</body></html>")
+});
+
+app.get('/professores', function(req, res){
+    res.send("<html><body>Professores da Fatec Sorocaba</body></html>")
+});
+
+app.listen(3000, function(){
+    console.log("Servidor com express foi carregado");
+});
